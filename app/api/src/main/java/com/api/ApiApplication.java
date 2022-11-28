69676e9848904999c100100cd40cc56e0bb48819
mymytre <sasha.suliz@gmail.com>
2022-11-28 16:52:55 +0100

    Created user auth with auto test user in db on start

--- app/api/src/main/java/com/api/ApiApplication.java
+++ app/api/src/main/java/com/api/ApiApplication.java
@@ -1,13 +1,32 @@
 package com.api;
 
+import com.api.entity.User;
+import com.api.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            PasswordEncoder passwordEncoder,
+            UserRepository userRepository
+    ) {
+        return args -> {
+            var user = new User(
+                    "test@test.com",
+                    passwordEncoder.encode("test")
+            );
+            userRepository.save(user);
+        };
     }
 
 }
