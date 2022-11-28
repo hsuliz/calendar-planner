8be66f3a2258476b4d2ef577d6f577a4cbb4ab9b
mymytre <sasha.suliz@gmail.com>
2022-11-28 16:59:26 +0100

    Test

---
+++ app/api/src/main/java/com/api/controller/ClientAuthController.java
@@ -0,0 +1,16 @@
+package com.api.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+@Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ClientAuthController {
+    // #TODO
+}
