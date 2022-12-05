3354a55c36eb2c5a90524f73aa816aa223a6b1ac
dkrzyzak <damntheguitar@gmail.com>
2022-12-05 16:48:14 +0100

    downgrade react to v17, install blueprint

--- app/front/src/App.tsx
+++ app/front/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import StartingPage from './pages/StartingPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ app/front/src/pages/LoginPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function LoginPage() {
+
+
+	return (
+        <div className='App'>
+            <header className='App-header'>
+                <p>
+                    Tekst jakiś
+                </p>
+            </header>
+        </div>
+	);
+}
+
+export default LoginPage;
