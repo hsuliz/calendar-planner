c15f45d9caed88b93030a518a9cdee37dfc904c6
mymytre <sasha.suliz@gmail.com>
2022-12-13 17:15:08 +0100

    login user

--- app/api/src/main/java/com/api/controller/UserAuthController.java
+++ app/api/src/main/java/com/api/controller/UserAuthController.java
@@ -1,6 +1,7 @@
 package com.api.controller;
 
 import com.api.entity.User;
+import com.api.model.LoginRequest;
 import com.api.service.UserAuthService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,10 +36,19 @@ public class UserAuthController {
             outJson.put("token", token);
         } catch (RuntimeException runtimeException) {
             outJson.put("failureReason", "User already exists!!");
-            return new ResponseEntity<>(outJson, HttpStatus.OK);
+            return new ResponseEntity<>(outJson, HttpStatusCode.valueOf(420));
         }
 
-        return new ResponseEntity<>(outJson, HttpStatusCode.valueOf(420));
+        return new ResponseEntity<>(outJson, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> createUsersToken(@RequestBody LoginRequest user) {
+        Map<String, String> outJson = new HashMap<>();
+
+        outJson.put("token", userAuthService.authenticateUser(user));
+
+        return new ResponseEntity<>(outJson, HttpStatus.OK);
     }
 
 }
