64dd2ac62da272d1a4e2a8f8a248cf6ebb6827a5
apocalypseDreamer <damntheguitars@gmail.com>
2022-12-15 18:10:19 +0100

    add list view

--- app/front/src/modules/CalendarView/CalendarView.tsx
+++ app/front/src/modules/CalendarView/CalendarView.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import FullCalendar, { EventContentArg } from '@fullcalendar/react';
+import FullCalendar, { EventClickArg } from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'; // needed for dayClick
 import { AddEventModal } from '../AddEventModal/AddEventModal';
 import './bootstrap-theme/bootstrap.min.css';
@@ -11,6 +12,13 @@ const CalendarView = () => {
 	const handleDateClick = (args: DateClickArg) => {
 		console.log(args);
 		setIsOpen(true);
+	};
+
+	const onEventClick = (eventInfo: EventClickArg) => {
+		console.log(eventInfo.event._def);
+		console.log(eventInfo.event.id);
+
+		// navigate to /:id
 	};
 
 	return (
@@ -24,38 +32,51 @@ const CalendarView = () => {
 				}}
 			>
 				<FullCalendar
-					plugins={[dayGridPlugin, interactionPlugin]}
-					// initialView="dayGridWeek"
+					plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
 					initialView='dayGridMonth'
 					events={[
-						{ title: 'event 1', date: '2022-12-01' },
+						{
+							title: 'event 1',
+							id: 'hash1',
+							start: '2022-12-01T16:00:00',
+							end: '2022-12-01T20:00:00',
+						},
 						{ title: 'event 2', date: '2022-12-02' },
 					]}
+					headerToolbar={{
+						left: 'prev,next today',
+						center: 'title',
+						right: 'dayGridMonth,dayGridWeek,listMonth',
+					}}
+					views={{
+						dayGridMonth: { buttonText: 'miesiąc' },
+						dayGridWeek: { buttonText: 'tydzień' },
+						listMonth: { buttonText: 'lista miesiąc' },
+					}}
 					dateClick={handleDateClick}
-					eventContent={renderEventContent}
+					eventClick={onEventClick}
 					locale={'pl'}
 					firstDay={1}
 					buttonText={{
 						today: 'powrót',
 					}}
 					themeSystem='bootstrap'
+					// eventContent={renderEventContent}
 				/>
 			</div>
 		</div>
 	);
 };
 
-function renderEventContent(eventInfo: EventContentArg) {
-	const onEventClick = () => {
-		console.log(eventInfo.event._def);
-	};
+// it may be useful at some point, now its not
 
-	return (
-		<div onClick={onEventClick}>
-			{/* <b>{eventInfo.timeText}</b> */}
-			<i>{eventInfo.event.title}</i>
-		</div>
-	);
-}
+// function renderEventContent(eventInfo: EventContentArg) {
+// 	return (
+// 		<div>
+// 			{/* <b>{eventInfo.timeText}</b> */}
+// 			<i>{eventInfo.event.title}</i>
+// 		</div>
+// 	);
+// }
 
 export default CalendarView;
